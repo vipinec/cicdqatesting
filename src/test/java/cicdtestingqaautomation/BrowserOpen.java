7d1a499e4b744f66c39b107976a11f8e6e9bddb6
mygitprojectw3learning <vipinec011@gmail.com>
2024-07-09 13:06:09 +0530

    first commit

---
+++ src/test/java/cicdtestingqaautomation/BrowserOpen.java
@@ -0,0 +1,35 @@
+package cicdtestingqaautomation;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BrowserOpen {
+	
+	
+	WebDriver driver;
+	
+	@Test
+	void openbro() {
+		
+		
+		WebDriverManager.chromedriver().setup();
+		
+		driver = new ChromeDriver();
+		
+		driver.get("https://facebook.com");
+		
+		
+		driver.manage().window().maximize();
+		
+		
+		driver.quit();
+		
+		
+		
+		
+	}
+
+}
