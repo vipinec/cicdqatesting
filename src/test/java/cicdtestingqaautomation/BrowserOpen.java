9614b180c9b00193bba93f9b3ea76937e816ab61
mygitprojectw3learning <vipinec011@gmail.com>
2024-07-14 02:10:27 +0530

    file updated for pox

--- src/test/java/cicdtestingqaautomation/BrowserOpen.java
+++ src/test/java/cicdtestingqaautomation/BrowserOpen.java
@@ -1,5 +1,6 @@
 package cicdtestingqaautomation;
 
+import org.testng.annotations.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
